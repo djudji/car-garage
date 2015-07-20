7a68cc2a32e63fc8a251fcc804aaf1b7bbe96d22
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2015-07-20 12:34:06 +0000

    Added validations

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,13 @@
 class Vehicle < ActiveRecord::Base
-  protokoll :lic_plate, :pattern => SecureRandom.hex(3).to_s+"-###%y-"+SecureRandom.hex(2).to_s
+  #before_create :generate_pattern
+  
+  protokoll :lic_plate, :pattern => SecureRandom.hex(2).to_s+"-###%y-"+SecureRandom.hex(2).to_s
+  #SecureRandom.hex(2).to_s+"-###%y-"+SecureRandom.hex(2).to_s
+
+  # def self.generate_pattern
+  #   begin
+  #       lic_plate = SecureRandom.hex(2).to_s+"-###%y-"+SecureRandom.hex(2).to_s
+  #   end while Vehicle.where(:lic_plate => lic_plate).exists?
+  #     self.lic_plate = lic_plate
+  # end
 end
